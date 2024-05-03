c3b6b5b5abecd926f8b36a61be9917ee4ca857d9
zleodai <awsomeleodai@gmail.com>
2024-05-03 16:40:04 -0700

    sad
    asd

--- src/myTeamTest.py
+++ src/myTeamTest.py
@@ -14,9 +14,10 @@ if __name__ == '__main__':
     
     start = time.time()
 
-    NUMBER_OF_GAMES = 30
     myAgents = capture.loadAgents(True, 'myTeam', False, '')
-    enemyAgents = capture.loadAgents(False, 'baselineTeam', False, '')
+
+    NUMBER_OF_GAMES = 100
+    enemyAgents = capture.loadAgents(False, 'myTeamOtherDupe', False, '')
     
     agents = [myAgents[0], enemyAgents[0], myAgents[1], enemyAgents[1]]
     
@@ -27,37 +28,9 @@ if __name__ == '__main__':
         layouts.append(l)
     
     optionsArgs = {'display': textDisplay.NullGraphics(), 'redTeamName': 'ALLIED FORCES', 'blueTeamName': 'ENEMIES', 'agents': agents, 'layouts': layouts, 'length': 1200, 'numGames': NUMBER_OF_GAMES, 'numTraining': 0, 'record': False, 'catchExceptions': False}
-    games = capture.runGames(**optionsArgs)
-
-    NUMBER_OF_GAMES = 20
-    enemyAgents = capture.loadAgents(False, 'betterBaselineTeam', False, '')
+    firstGame = capture.runGames(**optionsArgs)
     
-    agents = [myAgents[0], enemyAgents[0], myAgents[1], enemyAgents[1]]
-    
-    layouts = []
-    for i in range(NUMBER_OF_GAMES):
-        l = layout.getLayout('defaultCapture')
-
-        layouts.append(l)
-    
-    optionsArgs = {'display': textDisplay.NullGraphics(), 'redTeamName': 'ALLIED FORCES', 'blueTeamName': 'ENEMIES', 'agents': agents, 'layouts': layouts, 'length': 1200, 'numGames': NUMBER_OF_GAMES, 'numTraining': 0, 'record': False, 'catchExceptions': False}
-    games = capture.runGames(**optionsArgs)
-
-    NUMBER_OF_GAMES = 20
-    enemyAgents = capture.loadAgents(False, 'standoffTeam', False, '')
-    
-    agents = [myAgents[0], enemyAgents[0], myAgents[1], enemyAgents[1]]
-    
-    layouts = []
-    for i in range(NUMBER_OF_GAMES):
-        l = layout.getLayout('defaultCapture')
-
-        layouts.append(l)
-    
-    optionsArgs = {'display': textDisplay.NullGraphics(), 'redTeamName': 'ALLIED FORCES', 'blueTeamName': 'ENEMIES', 'agents': agents, 'layouts': layouts, 'length': 1200, 'numGames': NUMBER_OF_GAMES, 'numTraining': 0, 'record': False, 'catchExceptions': False}
-    games = capture.runGames(**optionsArgs)
-    
-    NUMBER_OF_GAMES = 20
+    NUMBER_OF_GAMES = 100
     enemyAgents = capture.loadAgents(False, 'myTeamDupe', False, '')
     
     agents = [myAgents[0], enemyAgents[0], myAgents[1], enemyAgents[1]]
@@ -69,7 +42,20 @@ if __name__ == '__main__':
         layouts.append(l)
     
     optionsArgs = {'display': textDisplay.NullGraphics(), 'redTeamName': 'ALLIED FORCES', 'blueTeamName': 'ENEMIES', 'agents': agents, 'layouts': layouts, 'length': 1200, 'numGames': NUMBER_OF_GAMES, 'numTraining': 0, 'record': False, 'catchExceptions': False}
-    games = capture.runGames(**optionsArgs)
+    secondGame = capture.runGames(**optionsArgs)
+    
+    totalGames = firstGame + secondGame
+    scores = [game.state.data.score for game in totalGames]
+    redWinRate = [s > 0 for s in scores].count(True) / float(len(scores))
+    blueWinRate = [s < 0 for s in scores].count(True) / float(len(scores))
+    print('\n\n\nAverage Score:', sum(scores) / float(len(scores)))
+    print('Scores:       ', ', '.join([str(score) for score in scores]))
+    print('Red Win Rate:  %d/%d (%.2f)' % (
+        [s > 0 for s in scores].count(True), len(scores), redWinRate))
+    print('Blue Win Rate: %d/%d (%.2f)' % (
+        [s < 0 for s in scores].count(True), len(scores), blueWinRate))
+    print('Record:       ', ', '.join(
+        [('Blue', 'Tie', 'Red')[max(0, min(2, 1 + s))] for s in scores]))
     
     timeTaken = time.time() - start
     
